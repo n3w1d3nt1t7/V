59e41255bd24424eef0021c5b80ce39fc6d96316
brega1 <binc.brega1@gmail.com>
2022-09-28 16:15:39 -0500

    Fixed rules command

--- commands/rules.js
+++ commands/rules.js
@@ -5,6 +5,6 @@ module.exports = {
         usage: `!rules`,
     },
     async run (bot,message,args) {
-        message.channel.send("**Rules**\n>1. No NSFW.\n2. Be mindful of what channel you are talking in. Respect chat perms and purposes.\n3. No spam.\n4. Respect your superiors. Address them properly.\n5. Do not complain about rank or placement.\n6. Maintain decent activity.\n7. Represent this force with maturity and professionalism.\n8. No insubordination.\n9. No leaking.\n10. Zero tolerance of discrimination or racism.");
+        message.channel.send("**Rules**\n> 1. No NSFW.\n> 2. Be mindful of what channel you are talking in. Respect chat perms and purposes.\n> 3. No spam.\n> 4. Respect your superiors. Address them properly.\n> 5. Do not complain about rank or placement.\n> 6. Maintain decent activity.\n> 7. Represent this force with maturity and professionalism.\n> 8. No insubordination.\n> 9. No leaking.\n> 10. Zero tolerance of discrimination or racism.");
     }
 }
