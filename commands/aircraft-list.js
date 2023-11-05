40fe207d25c3cf902aed01933f31b86db633df07
137177 <149951957+n3w1d3nt1t7@users.noreply.github.com>
2023-11-05 11:01:12 -0500

    Update aircraft-list.js

--- commands/aircraft-list.js
+++ commands/aircraft-list.js
@@ -12,12 +12,7 @@ module.exports = {
 	        .setColor(0x0099FF)
 	        .setTitle('Aircraft Used')
             .addFields(
-                { name: '@131st Fighter Wing', value: 'A10, F14, F15, F16' },
-                { name: '@80th Special Operations Wing', value: 'SR-71, B-2, A-400, MD-11, P-8, MG9B' },
-                { name: '@24th SAR Wing', value: 'UH-1, E-2, A-400' },
-                { name: '@525th Multi Wing', value: 'A-10, F-14, F-15, F-16, SR-71, B-2, A-400, \nMD-11, P-8, MG9B, UH-1, E-2' },
-                { name: '@177th UAV Surveillance Wing', value: 'MQ9B, SR-71' },
-                { name: '@10th Command Wing', value: 'Any Aircraft Above, F22' },
+                { name: 'null', value: 'null' },
             )
 
         message.channel.send( {embeds: [aircraftEmbed]} );
